696c329357d1fbfd47ca5b7b2eaaf54be6152750
Kristi <123495259+Kahelm621@users.noreply.github.com>
2024-02-15 11:57:44 -0600

    Update models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -2,26 +2,36 @@ from django.db import models
 from django.utils.timezone import now
 
 
-# Create your models here.
+# In djangoapp/models.py
 
-# <HINT> Create a Car Make model `class CarMake(models.Model)`:
-# - Name
-# - Description
-# - Any other fields you would like to include in car make model
-# - __str__ method to print a car make object
+from django.db import models
 
+# CarMake model
+class CarMake(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
 
-# <HINT> Create a Car Model model `class CarModel(models.Model):`:
-# - Many-To-One relationship to Car Make model (One Car Make has many Car Models, using ForeignKey field)
-# - Name
-# - Dealer id, used to refer a dealer created in cloudant database
-# - Type (CharField with a choices argument to provide limited choices such as Sedan, SUV, WAGON, etc.)
-# - Year (DateField)
-# - Any other fields you would like to include in car model
-# - __str__ method to print a car make object
+    # __str__ method to print a car make object
+    def __str__(self):
+        return self.name
 
+# CarModel model
+class CarModel(models.Model):
+    # Many-To-One relationship to CarMake model
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
+    dealer_id = models.IntegerField()  # Refers to a dealer created in Cloudant database
+    name = models.CharField(max_length=100)
+    SEDAN = 'SEDAN'
+    SUV = 'SUV'
+    WAGON = 'WAGON'
+    CAR_TYPE_CHOICES = [
+        (SEDAN, 'Sedan'),
+        (SUV, 'SUV'),
+        (WAGON, 'WAGON'),
+    ]
+    car_type = models.CharField(max_length=50, choices=CAR_TYPE_CHOICES)
+    year = models.DateField()
 
-# <HINT> Create a plain Python class `CarDealer` to hold dealer data
-
-
-# <HINT> Create a plain Python class `DealerReview` to hold review data
+    # __str__ method to print the car model object
+    def __str__(self):
+        return f"{self.car_make.name} - {self.name}"
